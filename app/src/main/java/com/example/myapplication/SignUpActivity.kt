b47066db025cfd40a7fad557dde9a84ada8caf2e
hamindo <dhm3235@naver.com>
2023-09-25 23:27:20 +0900

    Firebase 인증서비스 연동 완료

--- app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -1,11 +1,59 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //인증 초기화
+        mAuth = Firebase.auth
+
+        binding.signUpBtn.setOnClickListener{
+
+            val email = binding.emailEdit.text.toString().trim()
+            val password = binding.passwordEdit.text.toString().trim()
+
+            signUp(email, password)
+
+        }
     }
+
+
+
+    /**
+     * 회원 가입
+     */
+    private fun signUp(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // 성공시 실행
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    val intent: Intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // 실패시 실행
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+
 }
